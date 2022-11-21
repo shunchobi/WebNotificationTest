453aa7f6b76603f88d65c15c93fea9cde9b0fcee
kondo@qumu.co.jp <kondo@qumu.co.jp>
2022-11-21 20:51:24 +0900

    commit

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.get('/push', async (_req, res) => {
 });
 
 // サーバ起動
-const port = 8080;
-app.listen(port, () => {
-  console.log(new Date().toISOString(), `サーバ起動 : http://localhost:${port}/`);
-});
+// const port = 8080;
+// app.listen(port, () => {
+//   console.log(new Date().toISOString(), `サーバ起動 : http://localhost:${port}/`);
+// });
